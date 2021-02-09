b51bdaf30ef647d0850ec899717b95b9e8a56960
JAP <jean-anne.pipaud@ac-nantes.fr>
2021-02-09 19:30:54 +0100

    création des dossiers classe et élèves grâce à un form

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
+
+
 
 app = FastAPI()
 
@@ -9,7 +12,18 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+@app.get("/", response_class=HTMLResponse)
+async def affiche(request: Request):
+    return templates.TemplateResponse("item.html",{'request':request})
 
+@app.post("/", response_class=HTMLResponse)
+async def eleve_id(request: Request, nom: str = Form(...), prenom: str = Form(...), classe: str = Form(...)):
+    if not os.path.exists(classe):
+        os.mkdir(classe)
+    if not os.path.exists(classe + '/' + prenom + '.' + nom):
+        os.mkdir(classe + '/' + prenom + '.' + nom)
+    return templates.TemplateResponse("index.html",{'request':request, 'nom' : nom , 'prenom' : prenom, 'classe' : classe})
+
+@app.post("/remise", response_class=HTMLResponse)
+async def remise_fichier(fichier_1: UploadFile = File(...)):
+    return {"filename": fichier_1.filename}
