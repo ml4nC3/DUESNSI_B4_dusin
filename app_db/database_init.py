259a14f1b9e4cc8b2bf885111ed2539b1a60e2ee
ml4nC3 <nicolas.lance@stfelixlasalle.fr>
2021-02-11 17:49:10 +0100

    Correction du pluriel dans les nom de table

--- app_db/database_init.py
+++ app_db/database_init.py
@@ -28,23 +28,23 @@ create_table_noter = """CREATE TABLE IF NOT EXISTS `NOTER` (
   `note` VARCHAR(42),
   PRIMARY KEY (`id_eleve`, `id_eval`));"""
 
-create_table_evaluation = """CREATE TABLE IF NOT EXISTS `EVALUATION` (
+create_table_evaluations = """CREATE TABLE IF NOT EXISTS `EVALUATIONS` (
   `id_eval` VARCHAR(42),
   `date` VARCHAR(42),
   `criteres` VARCHAR(42),
   PRIMARY KEY (`id_eval`));"""
 
-alter_table_fichiers = "ALTER TABLE `FICHIERS` ADD FOREIGN KEY (`id_eleve`) REFERENCES `ELEVE` (`id_eleve`);"
-alter_table_eleves = "ALTER TABLE `ELEVE` ADD FOREIGN KEY (`id_classe`) REFERENCES `CLASSE` (`id_classe`);"
-alter_table_noter1 = "ALTER TABLE `NOTER` ADD FOREIGN KEY (`id_eval`) REFERENCES `EVALUATION` (`id_eval`);"
-alter_table_noter2 = "ALTER TABLE `NOTER` ADD FOREIGN KEY (`id_eleve`) REFERENCES `ELEVE` (`id_eleve`);"
+alter_table_fichiers = "ALTER TABLE `FICHIERS` ADD FOREIGN KEY (`id_eleve`) REFERENCES `ELEVES` (`id_eleve`);"
+alter_table_eleves = "ALTER TABLE `ELEVES` ADD FOREIGN KEY (`id_classe`) REFERENCES `CLASSES` (`id_classe`);"
+alter_table_noter1 = "ALTER TABLE `NOTER` ADD FOREIGN KEY (`id_eval`) REFERENCES `EVALUATIONS` (`id_eval`);"
+alter_table_noter2 = "ALTER TABLE `NOTER` ADD FOREIGN KEY (`id_eleve`) REFERENCES `ELEVES` (`id_eleve`);"
 
 requests = [
     create_table_classes,
     create_table_fichiers,
     create_table_eleves,
     create_table_noter,
-    create_table_evaluation,
+    create_table_evaluations,
     alter_table_fichiers,
     alter_table_eleves,
     alter_table_noter1,
