36b86a2cfceb49d1c0e1d0562231b95d34967ee0
ml4nC3 <nicolas.lance@stfelixlasalle.fr>
2021-02-11 18:03:43 +0100

    Ajout des auto-incrément sur les primary keys.

--- app_db/database_init.py
+++ app_db/database_init.py
@@ -2,39 +2,34 @@ import sqlite3
 from sqlite3 import Error
 
 create_table_classes = """CREATE TABLE IF NOT EXISTS `CLASSES` (
-  `id_classe` VARCHAR(42),
+  `id_classe` INTEGER PRIMARY KEY AUTOINCREMENT,
   `libelle` VARCHAR(42),
-  `niveau` VARCHAR(42),
-  PRIMARY KEY (`id_classe`));"""
+  `niveau` VARCHAR(42));"""
 
 create_table_eleves = """CREATE TABLE IF NOT EXISTS `ELEVES` (
-  `id_eleve` VARCHAR(42),
+  `id_eleve` INTEGER PRIMARY KEY AUTOINCREMENT,
   `nom` VARCHAR(42),
   `prenom` VARCHAR(42),
   `id_classe` VARCHAR(42),
-  PRIMARY KEY (`id_eleve`),
   FOREIGN KEY (`id_classe`) REFERENCES `CLASSES` (`id_classe`));"""
 
 create_table_fichiers = """CREATE TABLE IF NOT EXISTS `FICHIERS` (
-  `id_fichier` VARCHAR(42),
+  `id_fichier` INTEGER PRIMARY KEY AUTOINCREMENT,
   `empreinte` VARCHAR(42),
   `date_depot` VARCHAR(42),
   `chemin` VARCHAR(42),
   `id_eleve` VARCHAR(42),
-  PRIMARY KEY (`id_fichier`),
   FOREIGN KEY (`id_eleve`) REFERENCES `ELEVES` (`id_eleve`));"""
 
 create_table_evaluations = """CREATE TABLE IF NOT EXISTS `EVALUATIONS` (
-  `id_eval` VARCHAR(42),
+  `id_eval` INTEGER PRIMARY KEY AUTOINCREMENT,
   `date` VARCHAR(42),
-  `criteres` VARCHAR(42),
-  PRIMARY KEY (`id_eval`));"""
+  `criteres` VARCHAR(42));"""
 
 create_table_noter = """CREATE TABLE IF NOT EXISTS `NOTER` (
-  `id_eleve` VARCHAR(42),
+  `id_eleve` INTEGER PRIMARY KEY AUTOINCREMENT,
   `id_eval` VARCHAR(42),
   `note` VARCHAR(42),
-  PRIMARY KEY (`id_eleve`, `id_eval`),
   FOREIGN KEY (`id_eval`) REFERENCES `EVALUATIONS` (`id_eval`),
   FOREIGN KEY (`id_eleve`) REFERENCES `ELEVES` (`id_eleve`));"""
 
